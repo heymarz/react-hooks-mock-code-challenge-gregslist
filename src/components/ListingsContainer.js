fd40406ce9356e473d28c4199b9a1bab00fc2657
Marlena Tsang <Marlena345@gmail.com>
2021-12-14 10:00:11 -0500

    Completed Assignment

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,17 +1,33 @@
 import React, { useState, useEffect } from "react";
 import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
-  const [listings, setlistings] = useState([])
+function ListingsContainer({search}) {
+  const [listings, setListings] = useState([])
 
   useEffect(()=>{
     fetch("http://localhost:6001/listings")
     .then((r)=>r.json())
-    .then((listings)=>setlistings(listings))
+    .then((listings)=>setListings(listings))
   },[])
+  
+  function handleDelete(id){
+    const updatedArray= listings.filter(
+      (listing)=> listing.id !== id
+      );
+    setListings(updatedArray)
+  }
+
+  const filterListingArray = listings.filter((listing)=>{
+    return listing.description.toLowerCase().includes(search.toLowerCase())
+  })
+  console.log(filterListingArray)
 
   const listingCards = listings.map((listingObj)=>{
-    return <ListingCard key={listingObj.id} listing={listingObj} />
+    return <ListingCard 
+    key={listingObj.id} 
+    listing={listingObj}
+    onHandleDelete={handleDelete}
+    />
   })
   return (
     <main>
