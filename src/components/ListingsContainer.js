953c4e6aac806a06b1faa5e9e8d7f04db169e44c
Marlena Tsang <Marlena345@gmail.com>
2021-12-13 20:40:12 -0500

    toggle star done

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,22 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, { useState, useEffect } from "react";
+import ListingCard from "./ListingCard";
 
 function ListingsContainer() {
+  const [listings, setlistings] = useState([])
+
+  useEffect(()=>{
+    fetch("http://localhost:6001/listings")
+    .then((r)=>r.json())
+    .then((listings)=>setlistings(listings))
+  },[])
+
+  const listingCards = listings.map((listingObj)=>{
+    return <ListingCard key={listingObj.id} listing={listingObj} />
+  })
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {listingCards}
       </ul>
     </main>
   );
